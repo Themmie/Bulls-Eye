ec6229278596ec8909878ac6c98273e0a93cb9b6
Tyler Hemmie <tylerhemmie@Tylers-MacBook-Pro.local>
2014-02-23 22:43:47 -0800

    Added the button to the iPad and added the method for the button. Attached them.

--- Bulls Eye/ViewController.m
+++ Bulls Eye/ViewController.m
@@ -25,5 +25,11 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+- (IBAction)showAlert {
+    UIAlertView *alertView = [[UIAlertView alloc]
+                              initWithTitle:@"Hello, World"
+                              message:@"This is my first app!" delegate:nil
+                              cancelButtonTitle:@"Awesome"
+                              otherButtonTitles:nil];
+    [alertView show]; }
 @end
