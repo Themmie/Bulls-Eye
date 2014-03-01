5a717e7ff02ac8294ea015f994b94f0746de078c
Tyler Hemmie <tylerhemmie@Tylers-MacBook-Pro.local>
2014-03-01 14:30:42 -0800

    Well I added some incentives for getting close or right on the mark.

--- Bulls Eye/ViewController.m
+++ Bulls Eye/ViewController.m
@@ -44,16 +44,19 @@
     // Dispose of any resources that can be recreated.
 }
 - (IBAction)showAlert {
-    int difference = abs(_targetValue - _currentValue);int points = 100 - difference; _score += points;
+    int difference = abs(_targetValue - _currentValue);int points = 100 - difference;
     NSString *title;
-    if (difference == 0) { title = @"Perfect!";
+    if (difference == 0) { title = @"Perfect!"; points += 100;
     } else if (difference < 5) {
         title = @"You almost had it!";
-    } else if (difference < 10) {
+        if (difference == 1) {
+            points += 50; }
+    }else if (difference < 10) {
         title = @"Pretty good!";
     } else {
         title = @"Not even close...";
     }
+    _score += points;
     NSString *message = [NSString stringWithFormat:
                          @"You scored %d points", points];
     UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:title
