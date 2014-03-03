9ca69aab8e19237ae4794740c08e87de653c08fe
Tyler Hemmie <tylerhemmie@Tylers-MacBook-Pro.local>
2014-03-03 15:33:37 -0800

    Added i screen. Still working on it

---
+++ Bulls Eye/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  Bulls Eye
+//
+//  Created by Tyler Hemmie on 3/3/14.
+//  Copyright (c) 2014 Tyler Hemmie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
---
+++ Bulls Eye/AboutViewController.m
@@ -0,0 +1,38 @@
+//
+//  AboutViewController.m
+//  Bulls Eye
+//
+//  Created by Tyler Hemmie on 3/3/14.
+//  Copyright (c) 2014 Tyler Hemmie. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
